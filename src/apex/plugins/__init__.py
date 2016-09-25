4416bdc234066d0c2314622a47baa6cee16d1019
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2016-09-25 07:46:22 -0400

    Added a init file to the plugins directory.

---
+++ src/apex/plugins/__init__.py
@@ -0,0 +1,18 @@
+# These imports are for python3 compatability inside python2
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import copy
+import re
+import click
+
+import apex.aprs.util
+
+__author__ = 'Jeffrey Phillips Freeman (WI2ARD)'
+__maintainer__ = 'Jeffrey Phillips Freeman (WI2ARD)'
+__email__ = 'jeffrey.freeman@syncleus.com'
+__license__ = 'Apache License, Version 2.0'
+__copyright__ = 'Copyright 2016, Syncleus, Inc. and contributors'
+__credits__ = []
+__version__ = '0.0.2'
