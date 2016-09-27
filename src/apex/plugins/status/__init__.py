46a37fd65c3505b850d3cdcb88b5b88fca0d27c2
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2016-09-27 15:50:06 -0400

    Bumping version to 0.0.3

--- src/apex/plugins/status/__init__.py
+++ src/apex/plugins/status/__init__.py
@@ -13,7 +13,7 @@ __email__ = 'jeffrey.freeman@syncleus.com'
 __license__ = 'Apache License, Version 2.0'
 __copyright__ = 'Copyright 2016, Syncleus, Inc. and contributors'
 __credits__ = []
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 plugin = None
 
