c9f045e9560035bf7fc29baa3a1d1ee83b92e036
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2016-09-29 09:17:30 -0400

    Bumping version to 0.0.5

--- src/apex/__init__.py
+++ src/apex/__init__.py
@@ -18,4 +18,4 @@ __email__ = 'jeffrey.freeman@syncleus.com'
 __license__ = 'Apache License, Version 2.0'
 __copyright__ = 'Copyright 2016, Syncleus, Inc. and contributors'
 __credits__ = []
-__version__ = '0.0.4'
+__version__ = '0.0.5'
