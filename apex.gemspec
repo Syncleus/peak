817f429f725a8a4a137de7468c922c7affc95a62
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2016-10-01 17:15:36 -0400

    Removed version restrictions on bundler and rake

--- apex.gemspec
+++ apex.gemspec
@@ -30,8 +30,8 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency 'abstraction', '~> 0.0.4'
   spec.add_development_dependency 'json', '~> 1.8.3'
-  spec.add_development_dependency 'bundler', '~> 1.13'
-  spec.add_development_dependency 'rake', '~> 11.3.0'
+  spec.add_development_dependency 'bundler'
+  spec.add_development_dependency 'rake'
   spec.add_development_dependency 'rdoc'
   spec.add_development_dependency 'aruba'
   spec.add_development_dependency 'serialport'
--- lib/apex.rb
+++ lib/apex.rb
@@ -1,6 +1,20 @@
 require_relative 'apex/version'
-require_relative 'kiss/kiss_abstract'
+require_relative 'kiss/kiss_serial'
+require_relative 'aprs/aprs_kiss'
 
 module Apex
+    def self.main
+        kiss = Kiss::KissSerial.new('/dev/ttyUSB1')
+        aprs_kiss = Aprs::AprsKiss.new(kiss)
+        aprs_kiss.connect(Kiss::MODE_INIT_KENWOOD_D710)
 
+        while true
+            frame = aprs_kiss.read
+            if frame
+                p frame
+            else
+                sleep(1)
+            end
+        end
+    end
 end
