b3ff6cd3e7b0ca7ad59a375a4ee1406881633758
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2016-10-02 17:18:21 -0400

    Added echo output to the tnc port.

--- lib/apex/tnc_port.rb
+++ lib/apex/tnc_port.rb
@@ -1,7 +1,9 @@
+require 'apex/echo'
+
 module Apex
     class TncPort
         protected
-        def initialize(data_stream, name, identifier, net, port=0)
+        def initialize(data_stream, name, identifier, net, echo_frames=true, port=0)
             @data_stream = data_stream
             @port = port
             @name = name
@@ -21,12 +23,15 @@ module Apex
 
         public
         def read(*args, **kwargs)
-            return @data_stream.read(*args, **kwargs)
+            read_frame = @data_stream.read(*args, **kwargs)
+            echo_color_frame(read_frame, @name, true)
+            return read_frame
         end
 
         public
         def write(frame, *args, **kwargs)
             @data_stream.write(frame, @port, *args, **kwargs)
+            echo_color_frame(frame, @name, false)
         end
 
         attr_reader :name, :net, :port, :identifier
