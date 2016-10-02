7a3425dc828bfedb177f540b110b61ad6b615c31
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2016-10-02 18:31:28 -0400

    using new TcpPrt for reading

--- lib/apex.rb
+++ lib/apex.rb
@@ -10,14 +10,7 @@ module Apex
 
     def self.main
         config = find_config(true)
-        puts 'config:'
-        p config
-        puts
-        
-        puts 'port_map:'
         port_map = init_port_map(config)
-        p port_map
-        puts
         
         activated_plugins = []
         plugins = load_plugins
@@ -27,25 +20,22 @@ module Apex
             activated_plugin.run
         end
 
-        kiss = Kiss::KissSerial.new('/dev/ttyUSB1', 9600)
-        aprs_kiss = Aprs::AprsKiss.new(kiss)
-        aprs_kiss.connect(Kiss::MODE_INIT_KENWOOD_D710)
-
         # Transmit a beacon when we first start
         beacon_frame = {:source => 'WI2ARD-2',
                         :destination => 'APRS',
                         :path => ['WIDE1-1', 'WIDE2-2'],
                         :text => '!/:=i@;N.G& --PHG5790/G/D R-I-R H24 C30'}
-        aprs_kiss.write(beacon_frame)
-        echo_color_frame(beacon_frame, 'TNC', false)
+        port_map.values.each do |tnc_port|
+            tnc_port.write(beacon_frame)
+        end
 
         # Display any packets we read in.
         while true
-            frame = aprs_kiss.read
-            if frame
-                echo_color_frame(frame, 'TNC',  true)
-            else
-                sleep(1)
+            port_map.values.each do |tnc_port|
+                frame = tnc_port.read
+                unless frame
+                    sleep(1)
+                end
             end
         end
     end
