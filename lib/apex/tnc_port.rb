2ac04d7862a1435ab09c18660edd2b9760efbe62
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2016-10-02 18:35:38 -0400

    Fixed some bugs in TncPort

--- lib/apex/tnc_port.rb
+++ lib/apex/tnc_port.rb
@@ -3,12 +3,13 @@ require 'apex/echo'
 module Apex
     class TncPort
         protected
-        def initialize(data_stream, name, identifier, net, echo_frames=true, port=0)
+        def initialize(data_stream, name, identifier, net, echo_frames=false, port=0)
             @data_stream = data_stream
             @port = port
             @name = name
             @identifier = identifier
             @net = net
+            @ech_frames = echo_frames
         end
 
         public
@@ -24,14 +25,18 @@ module Apex
         public
         def read(*args, **kwargs)
             read_frame = @data_stream.read(*args, **kwargs)
-            echo_color_frame(read_frame, @name, true)
+            if @echo_frames
+                Apex::echo_color_frame(read_frame, @name, true)
+            end
             return read_frame
         end
 
         public
         def write(frame, *args, **kwargs)
             @data_stream.write(frame, @port, *args, **kwargs)
-            echo_color_frame(frame, @name, false)
+            if @echo_frames
+                Apex::echo_color_frame(frame, @name, false)
+            end
         end
 
         attr_reader :name, :net, :port, :identifier
